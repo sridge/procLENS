fcd5d2e34b43019bc3c713bf03692b1a5112e66c
sridge <smr1020@gmail.com>
2018-05-22 15:43:36 -0400

    LENS_filelist is the only working function

---
+++ LENS_proc_example.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+'/CCSM/csm/CESM-CAM5-BGC-LE/ocn/proc/tseries/' + freq + '/'
+memberList = [1, 2, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
+    20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 101, 102, 103, 104, 105]
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='procLENS',
+      version='0.1',
+      description='CESM-LENS pre-processing tools',
+      url='http://github.com/sridge/procLENS',
+      author='Sean Ridge',
+      author_email='sridge@ldeo.columbia.edu',
+      license='MIT',
+      packages=['procLENS'], 
+      zip_safe=False)
